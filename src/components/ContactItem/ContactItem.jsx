304abb4d07a00cb57a890245bc88e3167b952bad
OlenaLotos <olenameshanych@gmail.com>
2022-09-09 10:14:56 +0300

    some start

---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDeleteContactsMutation } from '../../Redux/ContactsApi';
+import { ItemLi, Button, Link, Img } from './ContactItem.styled';
+
+export const ContactItem = ({ id, name, number, avatar }) => {
+  const [deleteContact, { isLoading }] = useDeleteContactsMutation();
+
+  console.log(avatar);
+  const onDeleteContact = id => {
+    deleteContact(id);
+  };
+  return (
+    <ItemLi>
+      <Link href="tel:{number}">
+        <Img src={avatar} alt="avatar" width={35}></Img>
+        {name}: {number}
+      </Link>
+      <Button
+        type="button"
+        onClick={() => onDeleteContact(id)}
+        disabled={isLoading}
+      >
+        Delete
+      </Button>
+    </ItemLi>
+  );
+};
+
+ContactItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+};
